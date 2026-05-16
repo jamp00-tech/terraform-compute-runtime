8b492a3d744ea81c6707378f027bcc33b21f6a59
jamp <jamp@hotmail.com.com>
2026-05-16 17:00:01 -0300

    fix

--- envs/oci/dev/main.tf
+++ envs/oci/dev/main.tf
@@ -85,7 +85,7 @@ resource "oci_core_subnet" "app_subnet" {
 
   cidr_block   = var.app_subnet_cidr
   display_name = "${var.app_name}-subnet"
-  dns_label    = "appsubnet"
+  dns_label    = "${var.app_name}dns"
 
   route_table_id    = oci_core_route_table.app_route_table.id
   security_list_ids = [oci_core_security_list.app_security_list.id]
