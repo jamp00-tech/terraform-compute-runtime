80dbdf94c54db1a47fb577e5a1efc905dae428b0
jamp <jamp@hotmail.com.com>
2026-05-16 15:06:21 -0300

    fix

--- envs/oci/network/variables.tf
+++ envs/oci/network/variables.tf
@@ -9,3 +9,24 @@ variable "compartment_ocid" {
 variable "vcn_cidr_block" {
   type = string
 }
+
+variable "region" {
+  type = string
+}
+
+variable "tenancy_ocid" {
+  type = string
+}
+
+variable "user_ocid" {
+  type = string
+}
+
+variable "fingerprint" {
+  type = string
+}
+
+variable "private_key" {
+  type      = string
+  sensitive = true
+}
