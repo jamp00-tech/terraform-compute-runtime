337c996a5f5d4d9d8bd7e26bf2edf007683ac3b9
jamp <jamp@hotmail.com.com>
2026-05-16 14:46:00 -0300

    fix

--- envs/oci/network/providers.tf
+++ envs/oci/network/providers.tf
@@ -8,9 +8,5 @@ terraform {
 }
 
 provider "oci" {
-  region       = var.region
-  tenancy_ocid = var.tenancy_ocid
-  user_ocid    = var.user_ocid
-  fingerprint  = var.fingerprint
-  private_key  = var.private_key
+  config_file_profile = "DEFAULT"
 }
