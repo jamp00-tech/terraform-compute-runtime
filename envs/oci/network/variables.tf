6809a279ba7b7026c2af67ffdf629fc8a3193d59
jamp <jamp@hotmail.com.com>
2026-05-16 14:35:09 -0300

    fix variable name

--- envs/oci/network/variables.tf
+++ envs/oci/network/variables.tf
@@ -5,10 +5,3 @@ variable "env" {
 variable "compartment_ocid" {
   type = string
 }
-
-
-variable "region" { type = string }
-variable "tenancy_ocid" { type = string }
-variable "user_ocid" { type = string }
-variable "fingerprint" { type = string }
-variable "private_key" { type = string }
