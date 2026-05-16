30a3c1fc47f7bb8235bd881183b2ccf0a2a99ea0
jamp <jamp@hotmail.com.com>
2026-05-16 14:03:22 -0300

    Fix dns name format

--- envs/oci/network/main.tf
+++ envs/oci/network/main.tf
@@ -6,7 +6,7 @@ resource "oci_core_vcn" "main" {
   cidr_block     = "10.0.0.0/16"
   compartment_id = var.compartment_ocid
   display_name   = "${var.env}-vcn"
-  dns_label      = "dns-${var.env}-vcn"
+  dns_label      = "dns${var.env}vcn"
 }
 
 ############################################
